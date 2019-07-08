4166c9400f9b483bbf7118bd0a33c9ea2f32beb2
Ayman Ghaith <ayman.ghaith88@gmail.com>
2019-07-08 19:54:13 +0300

    Added front-end Vue client and connected to Django

---
+++ favorite_things/client/src/components/List.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="contatiner" id="fav-list">
+    <div class="row">
+      <div class="col-md-6 mx-auto">
+        <h1 class="text-center">
+          Favorite Things App
+        </h1>
+        <form v-on:submit.prevent="addNewFavThing">
+          <label for="favtitleinput">Title</label>
+          <input v-model="title" type="text" class="form-control" id="favtitleinput" placeholder="Add new favorite thing">
+          <label for="favdescinput">Description</label>
+          <input v-model="description" type="text" class="form-control" id="favdescinput" placeholder="Add description">
+          <label for="favrankinginput">Ranking</label>
+          <input v-model="ranking" type="number" class="form-control" id="favrankinginput" placeholder="Add Ranking">
+          <label for="favcategoryinput">Category</label>
+          <select v-model="category" name="category" class="form-control" id="favrankinginput">
+            <option v-for="(cat) in categories" v-bind:key="cat.id" v-bind:category="cat.name"> {{cat.name}}</option>
+            <option>Add new Category</option>
+          </select>
+          <button v-if="this.isEdit == false" type="submit" class="btn btn-success btn-block mt-3">
+            Submit
+          </button>
+          <button v-else v-on:click="updateFavThing()" type="button" class="btn btn-primary btn-block mt-3">
+            Updatefavthings
+          </button>
+
+          <table class="table">
+            <tr v-for="(fav) in favthings" v-bind:key="fav.id" v-bind:title="fav.title">
+              <td class="text-left"> {{fav.title}} </td>
+              <td class="text-right">
+                <button v-on:click="editFavThing(fav.title,fav.id)" type="button" class="btn btn-info"> Edit </button>
+                <button v-on:click="deleteFav(fav.id)" type="button" class="btn btn-danger"> Delete </button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      categories: [
+        {
+          'id': 1,
+          'name': 'cat1',
+          'description': 'cat1 desc'
+        },
+        {
+          'id': 2,
+          'name': 'cat2',
+          'description': 'cat2 desc'
+        }
+      ],
+      favthings: [],
+      id: '',
+      title: '',
+      isEdit: false
+    }
+  },
+  mounted () {
+    this.getFavThings()
+  },
+  methods: {
+    getFavThings () {
+      axios({method: 'GET', url: '/favoriteThings'}).then(
+        result => {
+          console.log(result.data)
+          this.favthings = result.data
+        },
+        error => {
+          console.error(error)
+        }
+      )
+    },
+    addNewFavThing () {
+      debugger
+      axios.post('/favoriteThings', {title: this.title}).then(
+        (res) => {
+          this.title = ''
+          this.getFavThings()
+          console.log(res)
+        }
+      ).catch(err => {
+        console.error(err)
+      })
+    },
+    editFavThing (title, id) {
+      this.id = id
+      this.title = title
+      this.isEdit = true
+    },
+    updateFavThing () {
+      axios.put(`/favoriteThings/${this.id}`, {title: this.title}).then(
+        res => {
+          this.title = ''
+          this.isEdit = false
+          this.getFavThings()
+          console.log(res)
+        }).catch(
+        err => {
+          console.error(err)
+        })
+    },
+    deleteFav (id) {
+      axios.delete(`/favoriteThings/${this.id}`).then(
+        res => {
+          this.title = ''
+          this.getFavThings()
+          console.log(res)
+        }).catch(
+        err => {
+          console.error(err)
+        })
+    }
+  }
+}
+</script>
